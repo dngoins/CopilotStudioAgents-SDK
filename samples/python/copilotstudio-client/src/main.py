96ba45f7130470eae6c223194a1e6739d563d00c
Dwight Goins <dngoins@hotmail.com>
2025-08-29 18:10:55 +0300

    Updated

--- samples/python/copilotstudio-client/src/main.py
+++ samples/python/copilotstudio-client/src/main.py
@@ -9,84 +9,22 @@ ms_agents_logger.addHandler(logging.StreamHandler())
 ms_agents_logger.setLevel(logging.INFO)
 
 import sys
-from os import environ
 import asyncio
-import webbrowser
 
 from dotenv import load_dotenv
 
-from msal import PublicClientApplication
-
 from microsoft_agents.activity import ActivityTypes, load_configuration_from_env
-from microsoft_agents.copilotstudio.client import (
-    ConnectionSettings,
-    CopilotClient,
-)
 
-from .local_token_cache import LocalTokenCache
+from .util.copilotstudioclient import CopilotStudioClient
 
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-TOKEN_CACHE = LocalTokenCache("./.local_token_cache.json")
-
-
-async def open_browser(url: str):
-    logger.debug(f"Opening browser at {url}")
-    await asyncio.get_event_loop().run_in_executor(None, lambda: webbrowser.open(url))
-
-
-def acquire_token(settings: ConnectionSettings, app_client_id, tenant_id):
-    pca = PublicClientApplication(
-        client_id=app_client_id,
-        authority=f"https://login.microsoftonline.com/{tenant_id}",
-        token_cache=TOKEN_CACHE,
-    )
-
-    token_request = {
-        "scopes": ["https://api.powerplatform.com/.default"],
-    }
-    accounts = pca.get_accounts()
-    retry_interactive = False
-    token = None
-    try:
-        if accounts:
-            response = pca.acquire_token_silent(
-                token_request["scopes"], account=accounts[0]
-            )
-            token = response.get("access_token")
-        else:
-            retry_interactive = True
-    except Exception as e:
-        retry_interactive = True
-        logger.error(
-            f"Error acquiring token silently: {e}. Going to attempt interactive login."
-        )
-
-    if retry_interactive:
-        logger.debug("Attempting interactive login...")
-        response = pca.acquire_token_interactive(**token_request)
-        token = response.get("access_token")
-
-    return token
-
 
 def create_client():
-    settings = ConnectionSettings(
-        environment_id=environ.get("COPILOTSTUDIOAGENT__ENVIRONMENTID"),
-        agent_identifier=environ.get("COPILOTSTUDIOAGENT__SCHEMANAME"),
-        cloud=None,
-        copilot_agent_type=None,
-        custom_power_platform_cloud=None,
-    )
-    token = acquire_token(
-        settings,
-        app_client_id=environ.get("COPILOTSTUDIOAGENT__AGENTAPPID"),
-        tenant_id=environ.get("COPILOTSTUDIOAGENT__TENANTID"),
-    )
-    copilot_client = CopilotClient(settings, token)
-    return copilot_client
+    client = CopilotStudioClient()
+    return client.client
 
 
 async def ainput(string: str) -> str:
@@ -113,6 +51,7 @@ async def ask_question(copilot_client, conversation_id):
                 print("\nEnd of conversation.")
                 sys.exit(0)
         await ask_question(copilot_client, conversation_id)
+
 
 
 async def main():
